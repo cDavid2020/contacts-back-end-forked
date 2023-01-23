bf651e391fb3d54603463df9feea0f97911a7115
Manav Misra <manavm@visionwebsoft.com>
2023-01-23 10:29:39 -0600

    Simplify the error message

--- app/contacts/controller.js
+++ app/contacts/controller.js
@@ -6,8 +6,12 @@ import config from "../config.js";
 mongoose
   .connect(config.getDbConn("contacts"))
   .then(() => {
-    console.log("Connected to the database");
+    console.info("Connected to the database");
   })
   .catch((err) => {
-    console.log("Error connecting to the database", err);
+    console.error("Error connecting to the database", err.message);
   });
+
+export default {
+  // TODO: Get all contacts
+};
