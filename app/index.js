889acdb5f685fb3e12ab790d4a5ffb50c4d9a541
Manav Misra <manavm@visionwebsoft.com>
2023-01-24 11:20:12 -0600

    :fire: Remove comments

--- app/index.js
+++ app/index.js
@@ -1,14 +1,9 @@
-// Express is a web framework for Node.js
-// We primarily use it to route users to different pages/responses
 import express from "express";
-import contactsRouter from "./contacts/routes.js";
+import contactsRouter from "./contact/routes.js";
 
 const app = express();
 
 app.use(express.json());
-
-// This is a middleware function
-// It will be called for every request
 app.use("/api/contacts", contactsRouter);
 
 app.listen(3000, () => {
