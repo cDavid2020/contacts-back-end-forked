78cefad4467e6cb2478ce82148ac98365bf5305c
Manav Misra <manavm@visionwebsoft.com>
2023-01-25 09:46:42 -0600

    Enforce strict query setting

--- app/contact/controller.js
+++ app/contact/controller.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import config from "../config.js";
 import Contact from "./Contact.js";
 
+// Enforce strict query mode to prevent queries for fields that don't exist in the schema
+mongoose.set("strictQuery", true);
+
 // Connect to the database
 mongoose
   .connect(config.dbConn)
