d1f3c441db87ac4a3d73015f0d1623be76bae343
Manav Misra <manavm@visionwebsoft.com>
2023-01-26 11:40:19 -0600

    feat: :bug: Update by id or name

--- app/contact/routes.js
+++ app/contact/routes.js
@@ -60,6 +60,39 @@ router.post("/", (request, response) => {
     });
 });
 
-// TODO: Implement routes to update and delete stuff in the controller
+router.put("/", async (request, response) => {
+  // The request can be made with either an ID or a username
+  // We will get this info from the query string
+  const { id, username } = request.query;
+
+  // The updated contact will be in the request body
+  const incomingContact = request.body;
+
+  // This will come from the controller
+  let updatedContact;
+
+  // If the ID is provided, we will use it to update the contact
+  if (id) {
+    updatedContact = await controller
+      .updateById(id, incomingContact)
+      .catch((err) => {
+        // TODO: Refactor this code to avoid duplication
+        if (err.message === "Invalid ID") {
+          response.status(400).json({ message: err.message });
+        } else {
+          response.status(500).json(err);
+        }
+      });
+  } else if (username) {
+    // If the username is provided, we will use it to update the contact
+    updatedContact = await controller
+      .updateByUsername(username, incomingContact)
+      .catch((err) => {
+        response.status(500).json(err);
+      });
+  }
+
+  if (updatedContact) response.json(updatedContact);
+});
 
 export default router;
