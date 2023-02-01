1d43000d5d46fbdeb9a9b38969382f9ce9357126
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-01-31 18:34:17 -0600

    Add notes to 'config.js'

--- app/config.js
+++ app/config.js
@@ -1,9 +1,16 @@
-given this code print it in code blocks. it includes // comments in the notes. explain specifically what each line is doing to an entry level javascript developer
-
+// Import the `dotenv` package
 import dotenv from "dotenv";
 
+// Load the environment variables defined in the .env file
 dotenv.config();
 
+// Export the `dbConn` object, which contains the value of the `DB_CONN` environment variable
 export default {
   dbConn: process.env.DB_CONN,
 };
+
+
+/*
+In this code, the dotenv library is imported, which allows the application to use environment variables. The dotenv.config() method is called, which loads the environment variables from a file named .env in the root directory of the application. The dbConn object is then exported, which contains the value of the DB_CONN environment variable.
+ */
+
--- app/contact/routes.js
+++ app/contact/routes.js
@@ -1,134 +1,76 @@
-given this code print it in code blocks. it includes // comments in the notes. explain specifically what each line is doing to an entry level javascript developer
-
+// Import the Router class from the express library
 import { Router } from "express";
+
+// Import the controller file which contains the logic for handling API requests
 import controller from "./controller.js";
 
+// Create a new instance of the Router class
 const router = new Router();
 
-// This will handle HTTP GET requests to /api/contacts
+// This will handle HTTP GET requests to the root route '/api/contacts'
 router.get("/", (_, response) => {
+  // Call the index() method on the controller
   controller
     .index()
     .then((contacts) => {
+      // If the index() method returns successfully, send the returned contacts data as a JSON response
       response.json(contacts);
     })
     .catch((err) => {
+      // If the index() method encounters an error, send a JSON response with a status code of 500 and the error message
       response.status(500).json(err);
     });
 });
 
+// This will handle HTTP GET requests to the route '/api/contacts/contact'
 router.get("/contact", async (request, response) => {
+  // Destructure the id and username from the query parameters in the request object
   const { id, username } = request.query;
 
   let contact;
 
+  // If the id is provided
   if (id) {
+    // Call the showById() method on the controller and catch any errors
     contact = await controller.showById(id).catch((err) => {
+      // If the error message is "Invalid ID", send a JSON response with a status code of 400 and the error message
       if (err.message === "Invalid ID") {
         response.status(400).json({ message: err.message });
       } else {
+        // If the error message is anything other than "Invalid ID", send a JSON response with a status code of 500 and the error message
         response.status(500).json(err);
       }
     });
-  } else if (username) {
+  }
+  // If the username is provided
+  else if (username) {
+    // Call the showByUsername() method on the controller and catch any errors
     contact = await controller.showByUsername(username).catch((err) => {
+      // Send a JSON response with a status code of 500 and the error message
       response.status(500).json(err);
     });
-  } else {
+  }
+  // If neither the id nor the username is provided
+  else {
+    // Send a JSON response with a status code of 400 and a message indicating that either the id or username is required
     return response.status(400).json({ message: "ID or username is required" });
   }
 
+  // If the contact is found
   if (contact) {
+    // Send a JSON response with the contact data
     response.json(contact);
   }
-
-  // * ⚠️ We need to explicitly compare with 'null'.
-  // * If we don't, the response will be sent twice.
-  // * 'null' means that the id was valid, but not found in the database.
+  // If the contact is not found and is explicitly set to null
   else if (contact === null) {
+    // Send a JSON response with a status code of 404 and a message indicating that the contact was not found
     response.status(404).json({ message: "Contact not found" });
   }
 });
 
+// This will handle HTTP POST requests to the root route '/api/contacts'
 router.post("/", (request, response) => {
+  // Get the new contact data from the request body
   const newContact = request.body;
 
-  controller
-    .create(newContact)
-    .then((createdContact) => {
-      response.status(201).json(createdContact);
-    })
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        return response.status(400).json(err.message);
-      }
-
-      response.status(500).json(err);
-    });
-});
-
-router.put("/", async (request, response) => {
-  const { id, username } = request.query;
-
-  const incomingContact = request.body;
-
-  let updatedContact;
-
-  if (id) {
-    updatedContact = await controller
-      .updateById(id, incomingContact)
-      .catch((err) => {
-        if (err.message.includes("ID")) {
-          response.status(400).json({ message: err.message });
-        } else if (err.name === "ValidationError" || err.name === "CastError") {
-          response.status(400).json(err.message);
-        } else if (err.path) {
-          response.status(400).json({
-            message: `Invalid request property in request body: ${err.path}`,
-          });
-        } else {
-          response.status(500).json(err);
-        }
-      });
-  } else if (username) {
-    // If the username is provided, we will use it to update the contact
-    updatedContact = await controller
-      .updateByUsername(username, incomingContact)
-      .catch((err) => {
-        response.status(500).json(err);
-      });
-  }
-
-  if (updatedContact) response.json(updatedContact);
-});
-
-router.delete("/", async (request, response) => {
-  const { id, username } = request.query;
-
-  let deletedContact;
-
-  if (id) {
-
-    deletedContact = await controller.deleteById(id).catch((err) => {
-      if (err.message === "Invalid ID") {
-        response.status(400).json({ message: err.message });
-      }
-
-      response.status(500).json(err);
-    });
-  } else if (username) {
-    deletedContact = await controller
-      .deleteByUsername(username)
-      .catch((err) => {
-        response.status(500).json(err);
-      });
-  }
-
-  if (deletedContact) {
-    response.json(deletedContact);
-  } else if (deletedContact === null) {
-    response.status(404).json({ message: "Contact not found" });
-  }
-});
-
-export default router;
+  // Call the create
