0208d42d5a05323f955fd92f2ca54a1db667209d
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-01-31 18:28:31 -0600

    Add to notes

--- app/contact/controller.js
+++ app/contact/controller.js
@@ -1,15 +1,12 @@
-given this code print it in code blocks. it includes // comments in the notes. explain specifically what each line is doing to an entry level javascript developer
-
-// THIS IS THE GATEWAY TO THE DATABASE
-// Mongoose
+// Importing the necessary modules and dependencies
 import mongoose from "mongoose";
 import config from "../config.js";
 import Contact from "./Contact.js";
 
-// Enforce strict query mode to prevent queries for fields that don't exist in the schema
+// Enforcing strict query mode to prevent errors while querying the database
 mongoose.set("strictQuery", true);
 
-// Connect to the database
+// Connecting to the database using the URL in the config file
 mongoose
   .connect(config.dbConn)
   .then(() => {
@@ -19,74 +16,71 @@ mongoose
     console.error("Error connecting to the database", err.message);
   });
 
-
-
+// Options for update methods
 const options = {
-  rawResult: true,
-  runValidators: true,
-  strict: "throw",
+  rawResult: true, // return the raw result of the query
+  runValidators: true, // validate the data before running the query
+  strict: "throw", // throw an error if a field is not defined in the schema
 };
 
+// Object with functions to handle contacts in the database
 const contactController = {
   // Get all contacts
   index() {
-    return Contact.find();
+    return Contact.find(); // Returns a promise with all the contacts in the database
   },
 
+  // Get a contact by ID
   showById(id) {
     if (mongoose.Types.ObjectId.isValid(id)) {
-      return Contact.findById(id);
+      return Contact.findById(id); // Returns a promise with the contact that matches the given ID
     }
 
-    // Wrap the error in a rejected promise so that it can be CAUGHT.
+    // Returns a rejected promise with an error message if the ID is invalid
     return Promise.reject(new Error("Invalid ID"));
   },
 
+  // Get a contact by username
   showByUsername(username) {
-    return Contact.findOne({ username });
+    return Contact.findOne({ username }); // Returns a promise with the contact that matches the given username
   },
 
+  // Create a new contact
   create(contact) {
-    return Contact.create(contact);
+    return Contact.create(contact); // Returns a promise with the newly created contact
   },
 
+  // Update a contact by ID
   updateById(id2Update, updatedContact) {
     if (updatedContact.id || updatedContact._id) {
+      // Returns a rejected promise with an error message if the ID field is present in the update data
       return Promise.reject(new Error("ID cannot be updated"));
     }
 
     if (mongoose.Types.ObjectId.isValid(id2Update)) {
+      // Update the contact that matches the given ID
       return Contact.findByIdAndUpdate(
         id2Update,
-
-        // The updated contact only needs to have the fields that are being updated
-        updatedContact,
+        updatedContact, // Data to update the contact with
         options
       );
     }
 
-    // Wrap the error in a rejected promise so that it can be CAUGHT.
-    // This is being AWAITed in the router (it expects a promise).
+    // Returns a rejected promise with an error message if the ID is invalid
     return Promise.reject(new Error("Invalid ID"));
   },
 
+  // Update a contact by username
   updateByUsername(username, updatedContact) {
-    return Contact.findOneAndUpdate({ username }, updatedContact, options);
+    return Contact.findOneAndUpdate({ username }, updatedContact, options); // Updates the contact that matches the given username
   },
 
+  // Delete a contact by ID
   deleteById(id2Delete) {
     if (mongoose.Types.ObjectId.isValid(id2Delete)) {
-      return Contact.findByIdAndDelete(id2Delete);
+      return Contact.findByIdAndDelete(id2Delete); // Deletes the contact that matches the given ID
     }
 
-    // Wrap the error in a rejected promise so that it can be CAUGHT.
+    // Returns a rejected promise with an error message if the ID is invalid
     return Promise.reject(new Error("Invalid ID"));
-  },
 
-  deleteByUsername(username) {
-    // * ⚠️ AVOID 'findOneAndDelete' because it kills random people
-    return Contact.deleteOne({ username }, options);
-  },
-};
-
-export default contactController;
